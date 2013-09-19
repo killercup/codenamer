87fa5d18040a0dc1fc315c01acc9e74d0c2fe834
Pascal Hertleif <killercup@gmail.com>
2013-09-19 15:06:27 +0200

    Add Basic Colors (Subset of Colors)

--- categories.js
+++ categories.js
@@ -234,7 +234,35 @@ var categories = {
     ]
   },
   "colors": {
-    "name": "Colors",
+    "name": "Colors (Basic)",
+    "source": "https://simple.wikipedia.org/wiki/List_of_colors",
+    "values": [
+      "Beige",
+      "Black",
+      "Blue",
+      "Bronze",
+      "Brown",
+      "Cyan",
+      "Golden",
+      "Gray",
+      "Green",
+      "Lilac",
+      "Magenta",
+      "Mauve",
+      "Olive",
+      "Orange",
+      "Pink",
+      "Purple",
+      "Red",
+      "Rose",
+      "Silver",
+      "Violet",
+      "White",
+      "Yellow"
+    ]
+  },
+  "colors_extended": {
+    "name": "Colors (Extended)",
     "source": "https://simple.wikipedia.org/wiki/List_of_colors",
     "values": [
       "Amaranth",
