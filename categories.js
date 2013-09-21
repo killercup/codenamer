bdab6ebf4805fb8c8cc069557914e95dfa67607b
Pascal Hertleif <killercup@gmail.com>
2013-09-21 14:20:44 +0200

    Add Kingdom Animalia
    https://en.wikipedia.org/wiki/Kingdom_(biology)

--- categories.js
+++ categories.js
@@ -1814,5 +1814,39 @@ var categories = {
       "poult",
       "wormlet"
     ]
+  },
+  "kingdom_animalia": {
+    "name": "Kingdom Animalia",
+    "source": "http://www.wolframalpha.com/share/clip?f=d41d8cd98f00b204e9800998ecf8427ekhnrbufb4a",
+    "values": [
+      "Acanthocephala",
+      "Annelida",
+      "Arthropoda",
+      "Brachiopoda",
+      "Cephalorhyncha",
+      "Chaetognatha",
+      "Cnidaria",
+      "Ctenophora",
+      "Cycliophora",
+      "Echinodermata",
+      "Echiura",
+      "Ectoprocta",
+      "Entoprocta",
+      "Gastrotricha",
+      "Gnathostomulida",
+      "Hemichordata",
+      "Mesozoa",
+      "Mollusca",
+      "Myxozoa",
+      "Nemata",
+      "Nemertea",
+      "Phoronida",
+      "Placozoa",
+      "Platyhelminthes",
+      "Porifera",
+      "Rotifera",
+      "Sipuncula",
+      "Tardigrada"
+    ]
   }
 };
